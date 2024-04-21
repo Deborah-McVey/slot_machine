01d917e51901c246fb70aabccdc6ba84a03f2a9c
Deborah McVey <deborah.mcvey@gmail.com>
2024-04-21 15:47:45 -0400

    prompt for user to enter deposit amount

---
+++ README.md
@@ -0,0 +1,20 @@
+"Learn JavaScript With This ONE Project!" by Tech With Tim
+https://www.youtube.com/watch?v=E3XxeE7NF30&t=565s
+
+https://github.com/techwithtim/JavaScript-Slot-Machine
+
+VS Code
+Node.js
+
+npm init to make package.json file
+
+npm i prompt-sync to make package-lock.json
+// collect user input
+
+// 1. deposit some money
+// 2. determine number of lines to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if user won
+// 6. give the user their winnings
+// 7. play again
---
+++ project.js
@@ -0,0 +1,22 @@
+// 1. deposit some money
+// 2. determine number of lines to bet on
+ // 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if use won
+// 6. give the user their winnings
+// 7. play again
+
+const prompt = require("prompt-sync") ();
+/* function deposit() {
+    return 1
+} */
+
+// const x = deposit()
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ")
+}
+
+deposit();
+
+// in terminal type node project.js, it will return Enter a deposit amount: 
+// you can enter an amount here
