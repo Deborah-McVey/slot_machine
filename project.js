3916684ae95d0ed8b853b31d1377f36d6cba3add
Deborah McVey <deborah.mcvey@gmail.com>
2024-04-21 16:23:35 -0400

    set up number of lines to bet on

--- project.js
+++ project.js
@@ -6,22 +6,50 @@
 // 6. give the user their winnings
 // 7. play again
 
-const prompt = require("prompt-sync") ();
+//step 1:
+
 /* function deposit() {
     return 1
-} */
+}
 
-// const x = deposit()
+const x = deposit() */
+
+const prompt = require("prompt-sync") ();
+
 const deposit = () => {
-    const depositAmount = prompt("Enter a deposit amount: ") // convert this string to a number
+    // infinite while loop until they enter a valid amount
+    while (true) {
+    const depositAmount = prompt("Enter a deposit amount: "); // convert this string to a number
     const numberDepositAmount = parseFloat(depositAmount);
 
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-        console.log("Invalid deposit amount, try again.");
-    } // run node project.js to test an invalid input
+        console.log("Invalid deposit amount, try again."); // run node project.js to test an invalid input
+    } else {
+        return numberDepositAmount;
+    }
+}
 };
 
-deposit();
+// const depositAmount = deposit(); // moved down
+//console.log(depositAmount);
 
 // in terminal type node project.js, it will return Enter a deposit amount: 
 // you can enter an amount here
+
+//step 2:
+// copy/paste and change variables
+const getNumberOfLines = () => {
+     while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
